e3789c1ca7c2cb71527855bf4257f3b170fd7374
Leandro Magalhaes <leandro.magalhaes@theiconic.com.au>
2020-01-20 11:57:27 +1100

    Rename build and deploy files to add .sh extension

---
+++ .cicd/build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+export TRACE=true
+source <(curl "${bamboo_cicd_api_url}/v3/")
+
+APP_NAME="superset"
+
+DEFAULT_DOCKERFILES_PATH="${cicd_working_directory}/theiconic/docker"
+CONTEXT_PATH="${cicd_working_directory}/theiconic"
+
+export DOCKER_BUILDKIT=1 \
+       BUILDKIT_PROGRESS=plain
+
+# Build nginx-redirect image
+APP_NGINX_IMAGE=$(cicd::get_docker_image "${APP_NAME}" "-nginx")
+docker build -t ${APP_NAME_NGINX} \
+    -f "${DEFAULT_DOCKERFILES_PATH}/Dockerfile-nginx-redirect" \
+    ${CONTEXT_PATH}
+
+# Build superset image
+APP_IMAGE=$(cicd::get_docker_image "${APP_NAME}")
+docker build -t ${APP_IMAGE} \
+    -f "${cicd_working_directory}/theiconic/docker/Dockerfile" \
+    ${CONTEXT_PATH}
+
+docker push ${APP_IMAGE}
+docker push ${APP_NGINX_IMAGE}
+
+
