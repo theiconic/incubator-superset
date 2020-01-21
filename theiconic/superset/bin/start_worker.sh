55373a3a7648943d7a822ee94ae7955aa77f7cce
Mitesh Chavda <mitesh.chavda@theiconic.com.au>
2020-01-21 14:46:47 +1100

    [DP-150] Fixed celery worker

--- theiconic/superset/bin/start_worker.sh
+++ theiconic/superset/bin/start_worker.sh
@@ -9,7 +9,7 @@ envsubst < /app/superset/data_okta.json.dist > /app/superset/data_okta.json
 envsubst < /app/superset/data_google.json.dist > /app/superset/data_google.json
 
 celery worker \
-       --app=superset.sql_lab:celery_app \
+       --app=superset.tasks.celery_app:app \
        --pool=gevent \
        -Ofair \
        --broker=amqp://$SUPERSET_AMQP_USER:$SUPERSET_AMQP_PASSWORD@$SUPERSET_AMQP_HOST:$SUPERSET_AMQP_PORT/$SUPERSET_AMQP_VHOST \
