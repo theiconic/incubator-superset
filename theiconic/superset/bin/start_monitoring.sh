3ce4efac9d83d6ab1d3dea213ce083a1815f6963
Mitesh Chavda <mitesh.chavda@theiconic.com.au>
2020-01-21 15:06:22 +1100

    [DP-150] Fixed monitoring

--- theiconic/superset/bin/start_monitoring.sh
+++ theiconic/superset/bin/start_monitoring.sh
@@ -8,4 +8,4 @@ export SUPERSET_CONFIG_PATH=/app/superset/superset_config.py
 envsubst < /app/superset/data_okta.json.dist > /app/superset/data_okta.json
 envsubst < /app/superset/data_google.json.dist > /app/superset/data_google.json
 
-celery flower --app=superset.sql_lab:celery_app --broker=amqp://${SUPERSET_AMQP_USER}:${SUPERSET_AMQP_PASSWORD}@${SUPERSET_AMQP_HOST}:${SUPERSET_AMQP_PORT}/${SUPERSET_AMQP_VHOST}
+celery flower --app=superset.tasks.celery_app:app --broker=amqp://${SUPERSET_AMQP_USER}:${SUPERSET_AMQP_PASSWORD}@${SUPERSET_AMQP_HOST}:${SUPERSET_AMQP_PORT}/${SUPERSET_AMQP_VHOST}
